05f5f1d66726504a416e820e439dc02eaa0852fb
zhangmengfei <18031835710@163.com>
2022-11-04 19:06:51 +0800

    mybatis练习2

--- Java/mybatis-test/mybatis-demo1/src/main/java/com/mffff/mybatis/mapper/UserMapper.java
+++ Java/mybatis-test/mybatis-demo1/src/main/java/com/mffff/mybatis/mapper/UserMapper.java
@@ -1,12 +1,24 @@
 package com.mffff.mybatis.mapper;
 
+import com.mffff.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface UserMapper {
 
     /**
      * 保持两个一致（通过mapper和mapper映射文件对比来看）：
      * 1.映射文件xml的namespace要和mapper接口的全类名保持一致
      * 2.定义的名称要和映射文件xml的id值保持一致
-     * @return
+     * @return int
      */
     int insertUser();
+
+    /**
+     * 获取用户列表
+     * @return List<User>
+     */
+    List<User> selectUsers();
+
 }
--- Java/mybatis-test/mybatis-demo1/src/test/java/com/mffff/mybatis/MybatisTest.java
+++ Java/mybatis-test/mybatis-demo1/src/test/java/com/mffff/mybatis/MybatisTest.java
@@ -1,6 +1,7 @@
 package com.mffff.mybatis;
 
 import com.mffff.mybatis.mapper.UserMapper;
+import com.mffff.mybatis.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MybatisTest {
 
@@ -27,5 +29,22 @@ public class MybatisTest {
         int i = mapper.insertUser();
         sqlSession.commit();
         System.out.println(i);
+    }
+
+    @Test
+    public void selectUsers() throws IOException {
+        // 字节流方法读取
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 构建工厂build
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 获取sqlsession工厂
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(resourceAsStream);
+        // 获取sqlsession（java和sql交互） 如果设置参数true代表开启自动提交事务
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 获取mapper
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.selectUsers();
+        // lambda表达式 (参数) -> {方法体}
+        users.forEach(System.out::println);
     }
 }
